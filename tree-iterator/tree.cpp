b032eb026745409547da5e63049b7585cc8fd23b
Alexey Kreshchuk <akrsch@gmail.com>
2021-04-28 11:12:54 +0300

    format and one more test

--- tree-iterator/tree.cpp
+++ tree-iterator/tree.cpp
@@ -1,20 +1,12 @@
 #include "tree.hpp"
 
-auto tree::iterator::operator++() -> iterator& {
-    return *this;
-}
+auto tree::iterator::operator++() -> iterator & { return *this; }
 
-auto tree::iterator::operator--() -> iterator& {
-    return *this;
-}
+auto tree::iterator::operator--() -> iterator & { return *this; }
 
-auto tree::begin() const -> iterator {
-    return {root.get()};
-}
+auto tree::begin() const -> iterator { return {root.get()}; }
 
-auto tree::end() const -> iterator {
-    return {root.get()};
-}
+auto tree::end() const -> iterator { return {root.get()}; }
 
 // Можно не менять
 auto tree::iterator::operator++(int) -> iterator {
